a582805c0e63f09f266c98f3c90eab7e000fc6c3
Roman SHamagin <roman.shamagin@7vpn.com>
2023-05-28 01:33:58 +0300

    fix

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -x
 # Проверка, что скрипт запущен от пользователя с правами суперпользователя
 if [[ $EUID -ne 0 ]]; then
    echo "Этот скрипт должен быть запущен от пользователя с правами суперпользователя (root)."
@@ -17,7 +18,10 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker $(whoami)
 rm get-docker.sh
+docker --version
 
 # Устанавливаем Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
